5f531b67a9daaba88c978e08c5f4d399dd1ec1da
Chetanpatel57 <40812586+Chetanpatel57@users.noreply.github.com>
2023-03-18 17:52:08 -0400

    Final commit

--- AnimationMoveApp/AnimationMoveApp/ViewController.swift
+++ AnimationMoveApp/AnimationMoveApp/ViewController.swift
@@ -15,11 +15,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        UIView.animate(withDuration: 2.0) {
-                self.myLabel.center.x += self.view.bounds.width
-                self.myTextField.center.x += self.view.bounds.width
-                self.myImageView.center.x += self.view.bounds.width
-            }
+        UIView.animate(withDuration: 2.0, delay: 0.5, usingSpringWithDamping: 0.1, initialSpringVelocity: 0.5, options: [.repeat, .autoreverse], animations: {
+            self.myLabel.center.x += self.view.bounds.width
+            self.myTextField.center.x += self.view.bounds.width
+            self.myImageView.center.x += self.view.bounds.width
+        }, completion: nil)
         func viewWillAppear(_ animated: Bool) {
             myLabel.center.x -= view.bounds.width
             myTextField.center.x -= view.bounds.width
