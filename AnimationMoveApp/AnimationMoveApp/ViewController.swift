29110f00d6a0c622c8b01d21d20d407a9885001a
Chetanpatel57 <40812586+Chetanpatel57@users.noreply.github.com>
2023-03-18 11:33:47 -0400

    Animation added

--- AnimationMoveApp/AnimationMoveApp/ViewController.swift
+++ AnimationMoveApp/AnimationMoveApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myLabel: UIView!
+    @IBOutlet var myTextField: UIView!
+    @IBOutlet var myImageView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        UIView.animate(withDuration: 2.0) {
+                self.myLabel.center.x += self.view.bounds.width
+                self.myTextField.center.x += self.view.bounds.width
+                self.myImageView.center.x += self.view.bounds.width
+            }
+        func viewWillAppear(_ animated: Bool) {
+            myLabel.center.x -= view.bounds.width
+            myTextField.center.x -= view.bounds.width
+            myImageView.center.x -= view.bounds.width
+        }
     }
-
-
 }
+
 
